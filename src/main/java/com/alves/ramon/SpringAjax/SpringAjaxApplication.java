8704c30781d44993c349a4567cfa683c440015b8
Ramon Alves <ramon.calou@hotmail.com>
2020-02-02 18:50:24 -0200

    Configuracao Inicial do DWR

--- src/main/java/com/alves/ramon/SpringAjax/SpringAjaxApplication.java
+++ src/main/java/com/alves/ramon/SpringAjax/SpringAjaxApplication.java
@@ -1,13 +1,28 @@
 package com.alves.ramon.SpringAjax;
 
+import org.directwebremoting.spring.DwrSpringServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
+@ImportResource(locations = "classpath: dwr-spring.xml")
 @SpringBootApplication
 public class SpringAjaxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAjaxApplication.class, args);
+	}
+	
+	@Bean
+	public ServletRegistrationBean<DwrSpringServlet> dwrSpringServlet(){
+		DwrSpringServlet dwrSpringServlet = new DwrSpringServlet();
+		
+		ServletRegistrationBean<DwrSpringServlet> registrationBean = new ServletRegistrationBean<>(dwrSpringServlet, "/dwr/*");
+		
+		registrationBean.addInitParameter("debug", "true");
+		registrationBean.addInitParameter("activeReverAjaxEnabled", "true");
+		return registrationBean;
 	}
 
 }
